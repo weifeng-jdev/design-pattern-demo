80336a85f4849651b707a313c3f4d2e112bb8342
卫峰 <im.wf@outlook.com>
2023-04-05 16:32:48 +0800

    feat:单例模式singleton

--- src/main/java/com/amano/design_pattern/creator/singleton/EnumSingleton.java
+++ src/main/java/com/amano/design_pattern/creator/singleton/EnumSingleton.java
@@ -3,7 +3,7 @@ package com.amano.design_pattern.creator.singleton;
 /**
  * @className: EnumSingleton
  * @package com.amano.design_pattern.creator.singleton
- * @description: TODO 类描述
+ * @description: 枚举实现单例模式
  * @author: amano
  * @date: 2023/4/5
  **/
